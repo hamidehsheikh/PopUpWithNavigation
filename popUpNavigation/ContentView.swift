e269daef587264c958fecb3e1acb6952b6e75791
hamidehsheikh <hamidehsheikh@gmail.com>
2022-05-28 02:15:44 +0430

    Initial Commit

---
+++ popUpNavigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  popUpNavigation
+//
+//  Created by Hamideh Sheikh on 5/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ popUpNavigation/popUpNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  popUpNavigationApp.swift
+//  popUpNavigation
+//
+//  Created by Hamideh Sheikh on 5/28/22.
+//
+
+import SwiftUI
+
+@main
+struct popUpNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
